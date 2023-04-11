13c828682106df0c10f4dbae94989f8dd54acef4
Alex Neep <alexander@thesolesupplier.co.uk>
2023-04-11 11:54:56 +0100

    Allow logged in access

--- app/routes/__auth.tsx
+++ app/routes/__auth.tsx
@@ -1,12 +1,14 @@
 import { getAuth } from "@clerk/remix/ssr.server";
 import { LoaderFunction, redirect } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
+import { json } from "react-router";
 
 export const loader: LoaderFunction = async (args) => {
   const { userId } = await getAuth(args);
   if (!userId) {
     return redirect("/login");
   }
+  return json({});
 };
 
 const AuthLayout = () => {
